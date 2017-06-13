b97397a73b35d02294d957f416f0ce619b56914e
shubham-padia <shubhamapadia@gmail.com>
2017-06-14 02:44:32 +0530

    quickfix: make relationships dasherized specifically for orga server
    The framework atm fetches the relationship name base on the url,
    which is wrong.Right now a quick fix to make them dasherized
    has been made. But, in future, this should be handled properly
    via marshmallow-jsonapi

--- flask_rest_jsonapi/resource.py
+++ flask_rest_jsonapi/resource.py
@@ -23,6 +23,10 @@ from flask_rest_jsonapi.data_layers.base import BaseDataLayer
 from flask_rest_jsonapi.data_layers.alchemy import SqlalchemyDataLayer
 
 
+def hyphenize(text):
+    return text.replace('-', '_')
+
+
 class ResourceMeta(MethodViewType):
 
     def __new__(cls, name, bases, d):
@@ -494,6 +498,7 @@ class ResourceRelationship(with_metaclass(ResourceMeta, Resource)):
         """Get useful data for relationship management
         """
         relationship_field = request.path.split('/')[-1]
+        relationship_field = hyphenize(relationship_field)
 
         if relationship_field not in get_relationships(self.schema).values():
             raise RelationNotFound('', "{} has no attribute {}".format(self.schema.__name__, relationship_field))
